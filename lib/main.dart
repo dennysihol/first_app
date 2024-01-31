248d9063262f0c0d2ee32863bd44a9ca5f8974dd
dennysihol <denny.sihol@gmail.com>
2024-01-31 18:01:51 +0700

    roll dice function not working

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import "package:flutter/material.dart";
 
-void main(){
-  runApp(MaterialApp());
+import "package:first_app/gradient_container.dart";
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(Color.fromARGB(255, 23, 91, 100), Color.fromARGB(31, 10, 207, 181)),
+      ),
+    ),
+  );
 }
+
 
